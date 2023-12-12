53b4c70e559c035eea508ba1e39dc46da259bdc1
Lisanne <lisanne.reilman@voys.nl>
2023-12-12 10:57:49 +0100

    added vite and lite-html and able to start with npm start

--- JS/guided-tour.js
+++ JS/guided-tour.js
@@ -1,78 +1,81 @@
-
 import { Overlay } from "./overlay.js";
 import { attachEventListeners } from "./events.js";
 
 export class GuidedTour {
-    constructor() {
-        this.tourSteps = document.querySelectorAll('*[data-intro]');
-        this.currentStepIndex = 0;
+  constructor() {
+    this.tourSteps = document.querySelectorAll("*[data-intro]");
+    this.currentStepIndex = 0;
 
-        // Create overlay 
-        this.overlay = new Overlay().overlay;
-        this.overlayContent = this.overlay.querySelector('.overlay-content');
+    // Create overlay
+    this.overlay = new Overlay().overlay;
+    this.overlayContent = this.overlay.querySelector(".overlay-content");
 
-        document.body.appendChild(this.overlay);
-        attachEventListeners(this);
+    document.body.appendChild(this.overlay);
+    attachEventListeners(this);
+  }
+
+  updateOverlayContent() {
+    // Remove highlight from previous step
+    this.tourSteps.forEach((step) => step.classList.remove("highlight"));
+
+    // Highlight the current step
+    this.tourSteps[this.currentStepIndex].classList.add("highlight");
+
+    // Use the current step element for positioning
+    const stepRect =
+      this.tourSteps[this.currentStepIndex].getBoundingClientRect();
+
+    const distance = 10;
+    this.overlay.style.top = `${stepRect.bottom + distance}px`;
+    this.overlay.style.left = `${stepRect.left + distance}px`;
+
+    // Update content
+    this.overlayContent.querySelector(".step-title").innerText =
+      this.tourSteps[this.currentStepIndex].getAttribute("data-title") || "";
+    this.overlayContent.querySelector(".step-text").innerText =
+      this.tourSteps[this.currentStepIndex].getAttribute("data-intro") ||
+      "Default Content";
+
+    // Disable Previous button if it's the first step
+    if (this.currentStepIndex === 0) {
+      this.overlay.querySelector(".prev-button").classList.add("disabled");
+    } else {
+      this.overlay.querySelector(".prev-button").classList.remove("disabled");
     }
 
-    updateOverlayContent() {
-        // Remove highlight from previous step
-        this.tourSteps.forEach(step => step.classList.remove('highlight'));
-    
-        // Highlight the current step
-        this.tourSteps[this.currentStepIndex].classList.add('highlight');
-    
-        // Use the current step element for positioning
-        const stepRect = this.tourSteps[this.currentStepIndex].getBoundingClientRect();
-    
-        const distance = 10;
-        this.overlay.style.top = `${stepRect.bottom + distance}px`;
-        this.overlay.style.left = `${stepRect.left + distance}px`;
-    
-        // Update content
-        this.overlayContent.querySelector('.step-title').innerText = this.tourSteps[this.currentStepIndex].getAttribute('data-title') || '';
-        this.overlayContent.querySelector('.step-text').innerText = this.tourSteps[this.currentStepIndex].getAttribute('data-intro') || 'Default Content';
+    // Progress bar
+    //this.overlay.updateProgressBar(this.currentStepIndex, this.tourSteps.length);
 
+    this.overlay.innerHTML = "";
+    this.overlay.appendChild(this.overlayContent);
+  }
 
-        // Disable Previous button if it's the first step
-        if (this.currentStepIndex === 0) {
-            this.overlay.querySelector('.prev-button').classList.add('disabled');
-        } else {
-            this.overlay.querySelector('.prev-button').classList.remove('disabled');
-        }
-    
-        // Logic for progress bar
-    
-        this.overlay.innerHTML = '';
-        this.overlay.appendChild(this.overlayContent);
+  nextStep() {
+    this.currentStepIndex++;
+    if (this.currentStepIndex >= this.tourSteps.length) {
+      this.endTour();
+      return;
     }
+    this.updateOverlayContent();
+  }
 
-    nextStep() {
-        this.currentStepIndex++;
-        if (this.currentStepIndex >= this.tourSteps.length) {
-            this.endTour();
-            return;
-        }
-        this.updateOverlayContent();
+  prevStep() {
+    this.currentStepIndex--;
+    if (this.currentStepIndex < 0) {
+      this.currentStepIndex = this.tourSteps.length - 1;
     }
-    
-    prevStep() {
-        this.currentStepIndex--;
-        if (this.currentStepIndex < 0) {
-            this.currentStepIndex = this.tourSteps.length - 1;
-        }
-        this.updateOverlayContent();
-    }
+    this.updateOverlayContent();
+  }
 
-    endTour() {
-        // Remove 'highlight' class from all tour steps
-        this.tourSteps.forEach(step => step.classList.remove('highlight'));
-        
-        this.overlay.style.display = 'none';
-    }
+  endTour() {
+    // Remove 'highlight' class from all tour steps
+    this.tourSteps.forEach((step) => step.classList.remove("highlight"));
 
-    start() {
-        this.overlay.style.display = 'flex';
-        this.updateOverlayContent();
-    }
+    this.overlay.style.display = "none";
+  }
+
+  start() {
+    this.overlay.style.display = "flex";
+    this.updateOverlayContent();
+  }
 }
